efeeb971db47b5b8110cf0ed1d879bee938be4b4
KarthikeyanRanasthala <karthikeyan.ranasthala@gmail.com>
2019-09-22 10:57:58 +0530

    Updates

---
+++ precache-manifest.8ed53e65a0b8ef0c63b3e84c86003058.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c864d731bf0353b0d25a2987a2065057",
+    "url": "/masai-week-7/index.html"
+  },
+  {
+    "revision": "74cc19157db3c7cafbad",
+    "url": "/masai-week-7/static/css/main.f7db0e15.chunk.css"
+  },
+  {
+    "revision": "78110804eca654b531f3",
+    "url": "/masai-week-7/static/js/2.bedab9cb.chunk.js"
+  },
+  {
+    "revision": "74cc19157db3c7cafbad",
+    "url": "/masai-week-7/static/js/main.fa436cb8.chunk.js"
+  },
+  {
+    "revision": "f2f9ca466f366b79ef4f",
+    "url": "/masai-week-7/static/js/runtime-main.e3e2bda6.js"
+  }
+]);
